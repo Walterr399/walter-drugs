c8d55897c3727dd0e41bf0a31bcb372725c51490
Walterr399 <walterwhiterp47@gmail.com>
2025-06-21 22:58:22 +0200

    Add respawntime

--- data/config.lua
+++ data/config.lua
@@ -4,22 +4,13 @@ Config.Framework = "ESX"
 
 Config.Configuration = {
     ["weed"] = {
-        location = vector3(222.5, -800.3, 30.7),
+        location = vector3(1618.9978, -219.5101, 258.1835),
         prop = "prop_plant_01a",
-        radius = 5,
+        radius = 25,
         item = "cannabis",
-        amount = math.random(1, 3),
-        minPlants = 3,
-        maxPlants = 6
-    },
-    ["coke"] = {
-        location = vector3(1500.0, -2000.0, 30.0),
-        prop = "prop_plant_01b",
-        radius = 5,
-        item = "coke_leaf",
-        amount = math.random(2, 4),
-        minPlants = 2,
-        maxPlants = 5
+        minPlants = 20,
+        maxPlants = 25,
+        respawnTime = 300
     }
 }
 
