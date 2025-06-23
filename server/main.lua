ab7e1a3785ad58366be5e0558edf9d838195fa21
Walterr399 <walterwhiterp47@gmail.com>
2025-06-23 18:04:23 +0200

    Added QBCore option

--- server/main.lua
+++ server/main.lua
@@ -1,25 +1,31 @@
 local Config = require("data.config")
 
+local ESX = nil
+local QBCore = nil
+
 if Config.Framework == "ESX" then
     ESX = exports["es_extended"]:getSharedObject()
+elseif Config.Framework == "QBCore" then
+    QBCore = exports["qb-core"]:GetCoreObject()
 end
 
 lib.callback.register("walter-drugs:server:reward:player", function(source, drugType)
     local src = source
-    local xPlayer = ESX.GetPlayerFromId(src)
-
-    if not xPlayer then
-        return false
-    end
-
     local data = Config.Configuration[drugType]
 
     if not data then
         return false
     end
 
-    exports.ox_inventory:AddItem(src, data.item, math.random(3, 8))
+    if Config.Framework == "ESX" then
+        local xPlayer = ESX.GetPlayerFromId(src)
+        if not xPlayer then return false end
+    elseif Config.Framework == "QBCore" then
+        local Player = QBCore.Functions.GetPlayer(src)
+        if not Player then return false end
+    end
 
+    exports.ox_inventory:AddItem(src, data.item, math.random(3, 8))
     return true
 end)
 
